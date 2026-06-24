8b31c61315f9eaafa58491dbabff86d0da401532
aldohl95 <mathias.tevita@icloud.com>
2026-06-24 00:55:37 -0700

    uploaded valid anagram solution

---
+++ arrays-hashing/ValidAnagram.py
@@ -0,0 +1,11 @@
+class Solution(self, s: str, t: str) -> bool:
+  return Counter(s) == Counter(t)
+
+class Solution2(self, s: str, t: str) -> bool:
+  countS, countT = {}, {}
+
+  for i in range(len(s)):
+    countS[s[i]] = 1 + countS.get(s[i], 0)
+    countT[t[i]] = 1 + countT.get(t[i],0)
+
+  return countS == countT
