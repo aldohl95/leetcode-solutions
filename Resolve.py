4b8114c3b7c351598520acb1df9c92611e817e9c
aldohl95 <mathias.tevita@icloud.com>
2026-06-26 12:36:35 -0700

    updated resolve que and log

--- Resolve.py
+++ Resolve.py
@@ -9,4 +9,39 @@ class Solution:
 def validAnagram(self, s: str, t: str) -> bool:
   return Counter(s) == Counter (t)
 
+def validPalindrome(self, s: str) -> bool:
+  l , r = 0, len(s)-1
 
+  while l < r:
+    while l < r and is not s[l].isalnum():
+      l += 1
+    while: r > l and is not s[r].isalnum():
+      r -= 1
+    if s[l].lower != s[r].lower:
+      return False
+    
+  return True
+def twoSum(self, nums: List[int], target: int) ->List[]:
+  seen = {}
+
+  for i in range(len(nums)):
+    complement = target - nums[i]:
+    if complement in seen:
+      return[seen[nums[i]] , i]
+    seen[complement] = i
+  
+  return []
+
+def binarySearch(self, nums: List[int], target: int) -> int:
+  l, r = 0, len(nums)-1
+
+  while l <= r:
+    mid = (l + r)//2
+    if nums[mid] == target:
+      return mid
+    if nums[mid] < target:
+      l = mid + 1
+    if nums[mid] > target:
+      r = mid - 1
+  
+  return -1
--- Resolve_Queue.md
+++ Resolve_Queue.md
@@ -1,9 +1,9 @@
 
 
 
-5. valid palindrome
+
 6. two sum
-7. binary search
+
 8. reverse linked list
 9. invert binary tree
 10. Best time to buy stocks
